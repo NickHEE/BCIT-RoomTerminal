bfb8325304556f690226bfe251507b7a8dc4a9a5
Nick <nick.huttemann@gmail.com>
2019-03-30 20:51:33 -0700

    1 stop bit

--- piTest.py
+++ piTest.py
@@ -1,11 +1,12 @@
 import serial
 
-UART = serial.Serial('/dev/ttyAMA0',stopbits=serial.STOPBITS_TWO, baudrate=115200, timeout=2)
+UART = serial.Serial('/dev/ttyAMA0', baudrate=115200, timeout=2)
 
 
 if __name__ == "__main__":
 	while True:
 		try:
+			#print(bin(ord(UART.read(1))))
 			print(UART.read(1))
 		except Exception as e:
 			print(e + '/n')
