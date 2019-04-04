1332492cd55de96cdfaa8f1ab2f59aad0c4ea5a8
luianyi <48133702+luianyi@users.noreply.github.com>
2019-04-04 15:55:17 -0700

    Add files via upload

--- FPGA/Keypad/doorlock.sv
+++ FPGA/Keypad/doorlock.sv
@@ -3,24 +3,48 @@
 
 
 module doorlock ( 
+					input unlock_signal,
 					input logic lock, 
 					input logic PB_state, 
 					input logic  unlock,  // need to be edge 
 					output logic lock_output); 
 			// 4 bit input num, and 8 bit output LEDS
 	
+	logic room_state; 
+	
 	always_comb begin 
 	
-		if (PB_state)
+		if (unlock_signal==1 && (room_state ==1)) 
 			lock_output = 1; 
-		else //if( unlock) 
-			lock_output = 0; 
+			
+			
+			
+			
+	
+	
+	
+	
+	
+		//if (PB_state)
+			//lock_output = 1; 
+		//else //if( unlock) 
+			//lock_output = 0; 
 	
 	
 	end 
 		
 endmodule 
 
+
+always_ff @ ( posedge available ) begin
+
+		
+		if ( unlock_signal )
+			lock_output =1;
+		
+	
+
+	
 
 
 	//  the program check is the room booked, if the room is booked, we can't do anything for now, 
