1e38ac8d3af17278c19ab5828447248452da471f
NickHEE <35848933+NickHEE@users.noreply.github.com>
2019-04-04 16:34:15 -0700

    Add files via upload

--- FPGA/Keypad/doorlock.sv
+++ FPGA/Keypad/doorlock.sv
@@ -2,52 +2,30 @@
 // control the door lock 
 
 
-module doorlock ( 
-					input unlock_signal,
-					input logic lock, 
-					input logic PB_state, 
-					input logic  unlock,  // need to be edge 
+module doorlock ( 	input logic FPGA_CLK1_50
+					input logic available,
+					input logic unavailable,
+					input logic unlock_signal
 					output logic lock_output); 
 			// 4 bit input num, and 8 bit output LEDS
 	
-	logic room_state; 
+	logic roomIsAvailable;
+	logic unlockState;
+	logic [28:0] div;
 	
-	always_comb begin 
-	
-		if (unlock_signal==1 && (room_state ==1)) 
-			lock_output = 1; 
-			
-			
-			
-			
-	
-	
-	
-	
-	
-		//if (PB_state)
-			//lock_output = 1; 
-		//else //if( unlock) 
-			//lock_output = 0; 
-	
-	
+	always_comb begin
+		if (available)
+			roomIsAvailable = 1;
+		if (unavailable)
+			roomIsAvailable = 0;
 	end 
-		
-endmodule 
 
-
-always_ff @ ( posedge available ) begin
-
-		
-		if ( unlock_signal )
-			lock_output =1;
-		
+always_ff @ ( posedge FPGA_CLK1_50 ) begin
+	case (unlockState)
+		0 : begin lock_output <= 0; div = 0; if (unlock_signal && roomIsAvailable ) unlockState <= 1 end //Locked Idle State
+		1 : begin lock_output <= 1; div <= div + 1; if (div >= 150000000) unlockState <= 0; end          //Unlock and wait 3 seconds
+		default : unlockState <= 0;
+end	
 	
-
-	
-
-
-	//  the program check is the room booked, if the room is booked, we can't do anything for now, 
-	// if the room is not booked, the student can pressed unlock to open the door 
-	
-	// at posedge book, telling the system that the room is booked and posedge clock of not_booked, 
+		
+			
