c07b9e3a0a60e96e0f5b3bf7688ec921dc2bf045
NickHEE <35848933+NickHEE@users.noreply.github.com>
2019-05-03 10:40:17 -0700

    formatting cleanup

--- FPGA/roomternimal/roomterminal.sv
+++ FPGA/roomternimal/roomterminal.sv
@@ -1,32 +1,15 @@
-// lock.sv - ELEX 7660 final project, this module's purpose is to 
-// control the door lock 
-
-
 module roomterminal ( output logic [3:0] kpc,  // column select, active-low
-					(* altera_attribute = "-name WEAK_PULL_UP_RESISTOR ON" *)
-					input logic  [3:0] kpr,  // rows, active-low w/ pull-ups
-					output logic [8:0] LED, // active-low LED segments 
-					output logic lock_output,
-					output logic TxD,  // column select, active-low
-					//output logic locktest,
-					output logic test,
-					 input logic available,
-					 input logic  reset_n,
-					 input logic FPGA_CLK1_50,
-					input logic unavailable);
-              
-		  
+		      (* altera_attribute = "-name WEAK_PULL_UP_RESISTOR ON" *)
+		      input logic  [3:0] kpr,  // rows, active-low w/ pull-ups
+		      output logic [8:0] LED, // active-low LED segments 
+		      output logic lock_output, TxD, test,
+		      input logic available, reset_n, FPGA_CLK1_50, unavailable);
+	  
 	logic TxD_start;
 	logic [7:0] TxD_data;
-	
-	
-			  
-			  
- 
+
 	keypad keypad_0(.*);		 
-	UART UART_0 (.*);		  
-				
-	
+	UART UART_0 (.*);	
 	
 endmodule
 
