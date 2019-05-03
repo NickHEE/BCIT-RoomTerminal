e8e418331183251d1f902f50847c42502315ce51
NickHEE <35848933+NickHEE@users.noreply.github.com>
2019-05-03 10:43:12 -0700

    cleanedup formatting

--- FPGA/roomternimal/kpdecode.sv
+++ FPGA/roomternimal/kpdecode.sv
@@ -9,59 +9,43 @@ module kpdecode ( input logic [3:0] kpc,   // 4 bit input kpc
 		output logic kphit,			// 1 bit output if any of the key is pressed
 		output logic [3:0] num );		// 4 bit output num to assign number pressed
 		
-always_comb begin
-	
-	/*
-	//if ( kpr == 'b1111) 		// if no key is pressed,
-	//	num = 0;				// output "no key is pressed"
-	//else 
-		//num = 1;    // or else the a key is pressed
-		
-	if ( kpr == 'b1011) 
-		num = 3;                // works when I physically connect 1011 to [3:0} kpr 
-	//else if (kpc == 'b1011)
-		//num = 4 ;
-	//else if (kpc == 'b1101)
-		//num = 5;
-	else num = 0;
-		*/
-		
+always_comb begin	
 	if ( kpr == 'b1111) 		// if no key is pressed,
-		kphit = 0;				// output "no key is pressed"
+		kphit = 0;		// output "no key is pressed"
 	else 
-		kphit = 1;    // or else the a key is pressed
-		
-	if ( kpc == 'b0111) 	// if the left most column (3rd) is pressed 
-		 case (kpr)			// to decode the position of key press
-			'b0111 : num = 1;	// 1st row
-			'b1011 : num = 4;	// 2nd row
-			'b1101 : num = 7;	// 3rd row
-			'b1110 : num = 14;	// 4th row
+		kphit = 1;              // or else the a key is pressed
+
+	if ( kpc == 'b0111) 	        // if the left most column (3rd) is pressed 
+		 case (kpr)			
+			'b0111 : num = 1;	
+			'b1011 : num = 4;	
+			'b1101 : num = 7;	
+			'b1110 : num = 14;	
 			default : num = 11;		
-								
+
 		endcase
-	
-	else if (kpc == 'b1011) // if the 2nd column is pressed 
+
+	else if (kpc == 'b1011)          // if the 2nd column is pressed 
 		 case (kpr)
-			'b0111 : num = 2;  // 1st row
+			'b0111 : num = 2;  
 			'b1011 : num = 5;
 			'b1101 : num = 8;
 			'b1110 : num = 0;
 		default : num =11;
 		endcase
-	
-	else if (kpc == 'b1101) // if the 2nd column is pressed 
+
+	else if (kpc == 'b1101)          // if the 3rd column is pressed 
 		 case (kpr)
-			'b0111 : num = 3; // 1st row
+			'b0111 : num = 3; 
 			'b1011 : num = 6;
 			'b1101 : num = 9;
 			'b1110 : num = 15;
 		default : num =11;
 		endcase
 
-	else if (kpc == 'b1110) // if the 2nd column is pressed 
+	else if (kpc == 'b1110)          // if the 4th column is pressed 
 		 case (kpr)
-			'b0111 : num = 10; // 1st row
+			'b0111 : num = 10; 
 			'b1011 : num = 11;
 			'b1101 : num = 12;
 			'b1110 : num = 13;
@@ -69,11 +53,7 @@ always_comb begin
 		endcase	
 	else 
 		num =11;                 
-	
-	end
-
-
-	
+end	
 endmodule 
 
 
